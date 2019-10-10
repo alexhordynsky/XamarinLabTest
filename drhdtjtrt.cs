624a83826bf1f7913c25470014838d3238336f60
alexhordynsky <56106310+alexhordynsky@users.noreply.github.com>
2019-10-10 08:11:40 +0200

    I have done the first exercise.

---
+++ drhdtjtrt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Open_Lab_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a < 10)
+
+
+            {
+                Console.WriteLine("This number is less then 10");
+            }
+            else
+            {
+                Console.WriteLine("This number is greater or equal to 10");
+
+                
+
+                
+
+
+                
+            }
+
+
+
+        }
+    }
+}
